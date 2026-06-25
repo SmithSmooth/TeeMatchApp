8bb97692abfafed85770016d0863047faa412c4c
Smith Ndungu <skn31@student.le.ac.uk>
2026-06-25 21:38:52 +0100

    Working to resolve issue db not found

--- js/server.js
+++ js/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-"/Users/smith/TeeMatchApp/database/db"//"/Users/smith/TeeMatchApp/database/db"
+const db=require("database/db")//"/Users/smith/TeeMatchApp/database/db"
 const { v4: uuidv4 } = require("uuid");
 
 
